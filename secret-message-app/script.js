fa42de3b17a12c738358c8e17113ec5079d9dfc5
Abbas <meerabbas110@gmail.com>
2023-10-19 19:51:25 +0530

    generating the encrypted message & select

--- secret-message-app/script.js
+++ secret-message-app/script.js
@@ -1,6 +1,10 @@
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const input = document.querySelector('input');
-  console.log(input.value);
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  const selected = document.querySelector('#link-input');
+  selected.value = `${window.location}#${encrypted}`;
+  selected.select();
 });
